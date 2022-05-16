ce839a9f6a64ab94e891a5d83bb0eda33f54924f
Muhammad Ziad Alfian <muhammadziadalfian07@gmail.com>
2022-05-16 23:40:26 +0800

    Remove Code Unused

--- lib/helper/date_helper.dart
+++ lib/helper/date_helper.dart
@@ -8,24 +8,6 @@ class DateHelper {
     return null;
   }
 
-  static String? changeFormatIdToYear(DateTime? date) {
-    if (date != null) {
-      return DateFormat('yyyy').format(date);
-    }
-    return null;
-  }
-
-  static String changeFormatIdToMonth(DateTime date) {
-    return DateFormat('MMMM').format(date);
-  }
-
-  static String? changeFormatYYYYMM({DateTime? date}) {
-    if (date != null) {
-      return DateFormat('yyyy/MM').format(date);
-    }
-    return null;
-  }
-
   static DateTime? convertStringToDateTime(String? date) {
     if (date != null) {
       return DateFormat('yyyy-MM-dd').parse(date);
